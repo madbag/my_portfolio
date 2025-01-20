6efc79084aa27dddb09eac8679c29d69a148622b
Madhushree B <boylemadhushree@gmail.com>
2025-01-20 19:36:06 +0100

    first commit

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,14 @@
+
+function Navbar() {
+  return (
+    <div>
+        <p>Home</p>
+        <p>Projects</p>
+        <p>Resume</p>
+        <p>About</p>
+        <p>Contact</p>
+    </div>
+  )
+}
+
+export default Navbar
