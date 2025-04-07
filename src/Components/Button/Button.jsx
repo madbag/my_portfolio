8b2300e8b67b5aff20cf93a29988b9c4b8dabafa
Madhushree B <boylemadhushree@gmail.com>
2025-04-07 19:24:43 +0200

    button color

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,14 +2,14 @@ export default function Button({ href, label }) {
   return (
     <div>
       <button className="px-[18px] py-[6px] rounded-[5px] text-[var(--hover-text-color)] hover:text-[var(--secondary-text-color)] cursor-pointer hover:bg-[var(--primary-text-color)] text-mt-[30px] bg-[var(--secondary-text-color)] border-1 border-[var(--hover-text-color)] ">
-        <span
+        <a
           href={href}
           target="_blank"
           rel="noopener noreferrer"
-          className="no-underline text-[14px]"
+          className="no-underline text-[14px] text-inherit"
         >
           {label}
-        </span>
+        </a>
       </button>
     </div>
   );
